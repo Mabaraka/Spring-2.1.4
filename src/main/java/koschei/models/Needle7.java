4511ad8ab31ec83ace10cac53c90454a94bf3d8d
Mabaraka <davimraba2020@gmail.com>
2021-07-19 05:34:41 +0900

    First solution 2.1.4 (clean code)

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Component;
 public class Needle7 {
 
     private Deth8 deth8;
+
+    public Deth8 getDeth8() {
+        return deth8;
+    }
+
     @Autowired
     public Needle7(Deth8 deth8) {
         this.deth8 = deth8;
